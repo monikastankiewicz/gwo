6f8875ae9b8889cab2944109042581d0df0b9ac9
Monika Stankiewicz <moniaastankiewicz@gmail.com>
2026-03-07 23:42:49 +0100

    T1: Add NelmioApiDocBundle and Twig for API documentation

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,6 @@ return [
     FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
